886c3187e54fd993f68cb096d85c77522b91b83c
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-04-24 18:37:08 -0300

    b-06 (#6)

---
+++ models/Manga.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose"
+
+let schema = new mongoose.Schema({
+    author_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'authors',
+        required: true
+    },
+    company_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'companies',
+        required: true
+    },
+    title: { type: String, required: true },
+    cover_photo: { type: String, required: true },
+    description: { type: String, required: true },
+    description: { type: String, required: true },
+    category_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'categories',
+        required: true
+    },
+}, {
+    time_stamps: true
+})
+
+let collection = 'mangas';
+
+let manga = mongoose.model(schema, collection);
+
+export default manga;
