e04ab948f3d31817e2861d90d56120358248d842
cartolanofacundo <cartolanofacundo@gmail.com>
2023-04-24 12:57:00 -0300

    b-03

---
+++ models/Author.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+let schema = new mongoose.Schema({
+    name: { type: String, required: true },
+    last_name: { type: String, required: false },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    date: { type: Date, required: false },
+    photo: { type: String, required: true },
+    user_id: { type: String, required: true },
+    active: { type: Boolean, required: true }
+}, {
+    time_stamps: true
+})
+
+let collection = 'authors';
+
+let author = mongoose.model(schema, collection);
+
+export default author;
