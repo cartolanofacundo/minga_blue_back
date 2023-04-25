bfd9f82830c6099553913b1e8796d532c830db9d
Facundo Cartolano <71531719+cartolanofacundo@users.noreply.github.com>
2023-04-24 23:00:17 -0300

    m-02 (#10)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
+import cors from 'cors'
 import { __dirname } from './utils.js';
 
 
@@ -15,7 +16,8 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+//middlewares
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
